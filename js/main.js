eab066ac4a837de5630cabf55487f7e43a0658f4
Tyler Cecchi <thcecchi@gmail.com>
2015-01-28 17:52:22 -0500

    third commit?

--- js/main.js
+++ js/main.js
@@ -7,8 +7,100 @@ var profilePage =
   },
 
   initStyle: function () {
+    profilePage.userProfileInfo();
+    profilePage.userRepoInformation();
 
-// user profile data
+
+
+// // user profile data
+//     var userAvatar = _.pluck([userData],"avatar_url");
+//     var userName = _.pluck([userData],"name");
+//     var userLogin = _.pluck([userData],"login");
+//     var userLocation = _.pluck([userData],"location");
+//     var userCreatedAt = _.pluck([userData],"created_at");
+//     var userFollowers = _.pluck([userData],"followers");
+//     var userFollowing = _.pluck([userData],"following");
+//
+//     var profData = {
+//       avatar_url: userAvatar,
+//       name: userName,
+//       login: userLogin,
+//       location: userLocation,
+//       created_at: userCreatedAt,
+//       followers: userFollowers,
+//       following: userFollowing
+//     };
+//
+//     var userCompiled = _.template(templates.userProf);
+//       console.log(userCompiled(profData));
+//       $("aside").append(userCompiled(profData));
+
+// // user repo data
+//     var repoName = _.pluck(repoData,"name");
+//     console.log(repoName);
+//     var repoUpdatedAt = _.pluck(repoData,"updated_at");
+//     console.log(repoUpdatedAt);
+//     var repoLanguage = _.pluck(repoData,"language");
+//     console.log(repoLanguage);
+//     var repoStargazersCount = _.pluck(repoData,"stargazers_count");
+//     console.log(repoStargazersCount);
+//     var repoForksCount = _.pluck(repoData,"forks_count");
+//     console.log(repoForksCount);
+//
+//     var repoFeedData = {
+//       name: repoName,
+//       updated_at: repoUpdatedAt,
+//       langauge: repoLanguage,
+//       stargazers_count: repoStargazersCount,
+//       forks_count: repoForksCount
+//     };
+//
+//     var repoCompiled = _.template(templates.repos);
+//       console.log(repoCompiled(repoFeedData));
+//       $(".repos").append(repoCompiled(repoFeedData));
+
+// // user activity data
+// activityData.forEach(function(Object) {
+//   var index = activityData.indexOf(Object);
+//   console.log(index);
+//
+//
+//     var activityAvatar = _.pluck(activityData[index], "avatar_url");
+//     console.log(activityAvatar);
+//     var activityName = _.pluck(activityData[index], "name");
+//     console.log(activityName);
+//     var activityLogin = _.pluck(activityData[index], "login");
+//     console.log(activityLogin);
+//     var activityRef = _.pluck(activityData[index], "ref");
+//     console.log(activityRef);
+//     var activityMessage = _.pluck(activityData[index], "message");
+//     console.log(activityMessage);
+//
+//     var activityFeedData = {
+//       avatar_url: activityAvatar,
+//       name: activityName,
+//       login: activityLogin,
+//       ref: activityRef,
+//       message: activityMessage
+//     };
+//
+//     var activitySmallCompiled = _.template(templates.activitySmall);
+//       console.log(activitySmallCompiled(activityFeedData));
+//       $(".activitySmall").append(activitySmallCompiled(activityFeedData));
+//
+//     // var activityLargeCompiled = _.template(templates.activityLarge);
+//     //   console.log(activityLargeCompiled(activityFeedData));
+//     //   $(".activityLarge").append(activityLargeCompiled(activityFeedData));
+// });
+
+  },
+
+  initEvents: function () {
+
+  },
+
+  userProfileInfo: function (element, index, array) {
+    // user profile data
     var userAvatar = _.pluck([userData],"avatar_url");
     var userName = _.pluck([userData],"name");
     var userLogin = _.pluck([userData],"login");
@@ -28,78 +120,81 @@ var profilePage =
     };
 
     var userCompiled = _.template(templates.userProf);
-
     console.log(userCompiled(profData));
-
     $("aside").append(userCompiled(profData));
 
-// user repo data
-    var repoName = _.pluck(repoData,"name");
-    console.log(repoName);
-    var repoUpdatedAt = _.pluck(repoData,"updated_at");
-    console.log(repoUpdatedAt);
-    var repoLanguage = _.pluck(repoData,"language");
-    console.log(repoLanguage);
-    var repoStargazersCount = _.pluck(repoData,"stargazers_count");
-    console.log(repoStargazersCount);
-    var repoForksCount = _.pluck(repoData,"forks_count");
-    console.log(repoForksCount);
+  },
 
-    var repoFeedData = {
-      name: repoName,
-      updated_at: repoUpdatedAt,
-      langauge: repoLanguage,
-      stargazers_count: repoStargazersCount,
-      forks_count: repoForksCount
-    };
+  addAllRepoData: function (repoData) {
 
-    var repoCompiled = _.template(templates.repos);
+  },
 
-    console.log(repoCompiled(repoFeedData));
+  userRepoInformation: function () {
 
-    $(".repos").append(repoCompiled(repoFeedData));
+    repoData.forEach(function(Object) {
+      var index = repoData.indexOf(Object);
 
-// user activity data
-    var activityAvatar = _.pluck(activityData,"avatar_url");
-    var activityName = _.pluck(activityData,"name");
-    var activityLogin = _.pluck(activityData,"login");
-    var activityRef = _.pluck(activityData,"ref");
-    var activityMessage = _.pluck(activityData,"message");
+      // user repo data
+      var repoName = _.pluck(repoData,"name");
+      console.log(repoName);
+      var repoUpdatedAt = _.pluck(repoData,"updated_at");
+      console.log(repoUpdatedAt);
+      var repoLanguage = _.pluck(repoData,"language");
+      console.log(repoLanguage);
+      var repoStargazersCount = _.pluck(repoData,"stargazers_count");
+      console.log(repoStargazersCount);
+      var repoForksCount = _.pluck(repoData,"forks_count");
+      console.log(repoForksCount);
 
-    var activityFeedData = {
-      avatar_url: activityAvatar,
-      name: activityName,
-      login: activityLogin,
-      ref: activityRef,
-      message: activityMessage
-    };
+      var repoFeedData = {
+        name: repoName[index],
+        updated_at: repoUpdatedAt[index],
+        langauge: repoLanguage[index],
+        stargazers_count: repoStargazersCount[index],
+        forks_count: repoForksCount[index]
+      };
 
-    var activitySmallCompiled = _.template(templates.activitySmall);
+      var repoCompiled = _.template(templates.repos);
+      console.log(repoCompiled(repoFeedData));
+      $(".repos").append(repoCompiled(repoFeedData));
+
+    });
+  },
+
+  userActivityFeed: function () {
+    // user activity data
+    activityData.forEach(function(Object) {
+      var index = activityData.indexOf(Object);
+      console.log(index);
+
+
+      var activityAvatar = _.pluck(activityData[index], "avatar_url");
+      console.log(activityAvatar);
+      var activityName = _.pluck(activityData[index], "name");
+      console.log(activityName);
+      var activityLogin = _.pluck(activityData[index], "login");
+      console.log(activityLogin);
+      var activityRef = _.pluck(activityData[index], "ref");
+      console.log(activityRef);
+      var activityMessage = _.pluck(activityData[index], "message");
+      console.log(activityMessage);
+
+      var activityFeedData = {
+        avatar_url: activityAvatar,
+        name: activityName,
+        login: activityLogin,
+        ref: activityRef,
+        message: activityMessage
+      };
+
+      var activitySmallCompiled = _.template(templates.activitySmall);
       console.log(activitySmallCompiled(activityFeedData));
-      $(".activity").append(activitySmallCompiled(activityFeedData));
+      $(".activitySmall").append(activitySmallCompiled(activityFeedData));
 
-    var activityLargeCompiled = _.template(templates.activityLarge);
-      console.log(activityLargeCompiled(activityFeedData));
-      $(".activity").append(activityLargeCompiled(activityFeedData));
-
-  },
-
-  initEvents: function () {
-
-  },
-
-  addCommit: function (element, index, array) {
-
-
-  },
-
-  addAllCommits: function (profileData) {
-    // userData.forEach(profilePage.addCommits);
-  },
-
-  sideBarProfile: function (userData) {
-
-    
+      // var activityLargeCompiled = _.template(templates.activityLarge);
+      //   console.log(activityLargeCompiled(activityFeedData));
+      //   $(".activityLarge").append(activityLargeCompiled(activityFeedData));
+    });
 
   },
 
--- templates/templates.js
+++ templates/templates.js
@@ -1,13 +1,19 @@
 var templates = {};
 
 templates.userProf = [
-  "<img src = <%= avatar_url %>",
+  "<img src = <%= avatar_url %>>",
+  "<div class='userInformation'>",
   "<h1><%= name %></h1>",
   "<h3><%= login %></h3>",
   "<p><%= location %></p>",
   "<p>Joined</p><p><%= created_at %></p>",
-  "<p>Followers</p><h2><%= followers %></h2>",
-  "<p>Following</p><h2><%= following %></h2>"
+  "</div>",
+  "<div class='stats'>",
+  "<p class='number'>Followers</p><h2><%= followers %></h2>",
+  "</div>",
+  "<div class='stats'>",
+  "<p class='number'>Following</p><h2><%= following %></h2>",
+  "</div>"
 ].join("")
 
 templates.repos = [
@@ -24,24 +30,24 @@ templates.repos = [
 
 templates.activitySmall = [
   "<div class='activityName'>",
-  "<span>ICON</span>",
+  "<span class='mega-octicon octicon-git-commit'></span>",
   "<p><%= login %></p>",
   "<p>created branch</p>",
-  "<span>ICON</span>",
+  "<span class='octicon octicon-git-branch'></span>",
   "<p>at</p>",
   "<p><%= name %></p>",
   "</div>"
 ].join("")
 
-templates.activityLarge = [
-"<div>",
-"<span>ICON</span>",
-"<img src = <%= avatar_url %>",
-"<p><%= login %></p>",
-"<p>pushed to</p>",
-"<p><%= ref %></p>",
-"<p>at</p>",
-"<p><%= name %></p>",
-"<p><%= message %></p>",
-"</div>"
-].join("")
+// templates.activityLarge = [
+// "<div>",
+// "<span class='mega-octicon octicon-git-branch'></span>",
+// "<img src = <%= avatar_url %>",
+// "<p><%= login %></p>",
+// "<p>pushed to</p>",
+// "<p><%= ref %></p>",
+// "<p>at</p>",
+// "<p><%= name %></p>",
+// "<p><%= message %></p>",
+// "</div>"
+// ].join("")
